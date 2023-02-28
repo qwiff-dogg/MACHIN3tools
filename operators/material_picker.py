40c52ada2657ab1c620a58a3611ba4999d5c449c
machin3io <social@machin3.io>
2023-02-28 16:24:48 +0100

    MaterialPicker() - support assigning materials from the asset browser (1)

--- operators/material_picker.py
+++ operators/material_picker.py
@@ -3,9 +3,12 @@ from bpy.props import BoolProperty
 from bl_ui.space_statusbar import STATUSBAR_HT_header as statusbar
 from mathutils import Vector
 from .. utils.raycast import cast_obj_ray_from_mouse, cast_bvh_ray_from_mouse
-from .. utils.draw import draw_label
+from .. utils.draw import draw_label, update_HUD_location
 from .. utils.registration import get_prefs
-from .. items import alt
+from .. utils.system import printd
+from .. utils.ui import init_cursor
+from .. items import alt, ctrl
+from .. colors import white, yellow, green
 
 
 def draw_material_pick_status(self, context):
@@ -35,6 +38,9 @@ class MaterialPicker(bpy.types.Operator):
     bl_description = "Pick a Material from the 3D View\nALT: Assign it to the Selection too"
     bl_options = {'REGISTER', 'UNDO'}
 
+    # hidden
+    passthrough = None
+
     @classmethod
     def poll(cls, context):
         return context.area.type == 'VIEW_3D'
@@ -42,61 +48,119 @@ class MaterialPicker(bpy.types.Operator):
     def draw_HUD(self, args):
         context, event = args
 
-        draw_label(context, title="Assign" if self.is_assign else "Pick", coords=self.mousepos + Vector((20, 10)), center=False)
+        title, color = ("Assign from Asset Browser", green) if self.assign_from_assetbrowser else ("Assign", yellow) if self.assign else ("Pick", white)
+        draw_label(context, title=title, coords=Vector((self.HUD_x, self.HUD_y)), color=color, center=False)
+
 
     def modal(self, context, event):
         context.area.tag_redraw()
 
         self.mousepos = Vector((event.mouse_region_x, event.mouse_region_y))
-        self.is_assign = event.alt
-
-        if event.type == 'LEFTMOUSE':
-            if context.mode == 'OBJECT':
-                hitobj, hitobj_eval, _, _, hitindex, _ = cast_obj_ray_from_mouse(self.mousepos, depsgraph=self.dg, debug=False)
-
-            elif context.mode == 'EDIT_MESH':
-                hitobj, _, _, hitindex, _, _ = cast_bvh_ray_from_mouse(self.mousepos, candidates=[obj for obj in context.visible_objects if obj.mode == 'EDIT'])
-
-            if hitobj:
-                if context.mode == 'OBJECT':
-                    matindex = hitobj_eval.data.polygons[hitindex].material_index
-                elif context.mode == 'EDIT_MESH':
-                    matindex = hitobj.data.polygons[hitindex].material_index
-
-                context.view_layer.objects.active = hitobj
-                hitobj.active_material_index = matindex
-
-                if hitobj.material_slots and hitobj.material_slots[matindex].material:
-                    mat = hitobj.material_slots[matindex].material
-
-                    if self.is_assign:
-                        sel = [obj for obj in context.selected_objects if obj != hitobj and obj.data]
-
-                        for obj in sel:
-                            if not obj.material_slots:
-                                obj.data.materials.append(mat)
-
-                            else:
-                                obj.material_slots[obj.active_material_index].material = mat
+        self.mousepos_screen = Vector((event.mouse_x, event.mouse_y))
 
 
-                    bpy.ops.machin3.draw_label(text=mat.name, coords=self.mousepos, alpha=1, time=get_prefs().HUD_fade_material_picker)
+        area_under_mouse = self.get_area_under_mouse(self.mousepos_screen)
+        # print("area under mouse:", area_under_mouse)
 
-                else:
-                    bpy.ops.machin3.draw_label(text="Empty", coords=self.mousepos, color=(0.5, 0.5, 0.5), alpha=1, time=get_prefs().HUD_fade_material_picker + 0.2)
+        # restore mouse eyedropper mouse cursor, and fetch the selected asset from the asset browser
+        if self.passthrough and area_under_mouse != 'ASSET_BROWSER':
+            self.passthrough = False
+            context.window.cursor_set("EYEDROPPER")
 
-            else:
-                bpy.ops.machin3.draw_label(text="None", coords=self.mousepos, color=(1, 0, 0), alpha=1, time=get_prefs().HUD_fade_material_picker + 0.2)
+            # fetch selected asset from asset browser
+            self.asset_browser_material = self.get_selected_material()
 
-            self.finish(context)
-            return {'FINISHED'}
+            if self.asset_browser_material:
+                printd(self.asset_browser_material)
 
-        elif event.type == 'MIDDLEMOUSE':
+
+        # PASSTROUGH to ASSET BROWSER
+
+        if area_under_mouse == 'ASSET_BROWSER':
+            self.passthrough = True
             return {'PASS_THROUGH'}
 
-        elif event.type in ['RIGHTMOUSE', 'ESC']:
-            self.finish(context)
-            return {'CANCELLED'}
+
+        # VIEW3D
+
+        elif area_under_mouse == 'VIEW_3D':
+
+            # MOD KEYS
+
+            self.assign = event.alt
+
+            if 'ASSET_BROWSER' in self.areas:
+                self.assign_from_assetbrowser = event.ctrl
+
+            if event.type in [*alt, *ctrl]:
+                if event.value == 'PRESS':
+                    context.window.cursor_set("PAINT_CROSS")
+
+                elif event.value == 'RELEASE':
+                    context.window.cursor_set("EYEDROPPER")
+
+
+            # MOUSEMOVE
+
+            if event.type == 'MOUSEMOVE':
+                update_HUD_location(self, event)
+
+
+            # FINISH
+
+            elif event.type == 'LEFTMOUSE':
+                if context.mode == 'OBJECT':
+                    hitobj, hitobj_eval, _, _, hitindex, _ = cast_obj_ray_from_mouse(self.mousepos, depsgraph=self.dg, debug=False)
+
+                elif context.mode == 'EDIT_MESH':
+                    hitobj, _, _, hitindex, _, _ = cast_bvh_ray_from_mouse(self.mousepos, candidates=[obj for obj in context.visible_objects if obj.mode == 'EDIT'])
+
+                if hitobj:
+                    if context.mode == 'OBJECT':
+                        matindex = hitobj_eval.data.polygons[hitindex].material_index
+                    elif context.mode == 'EDIT_MESH':
+                        matindex = hitobj.data.polygons[hitindex].material_index
+
+                    context.view_layer.objects.active = hitobj
+                    hitobj.active_material_index = matindex
+
+                    if hitobj.material_slots and hitobj.material_slots[matindex].material:
+                        mat = hitobj.material_slots[matindex].material
+
+                        if self.assign:
+                            sel = [obj for obj in context.selected_objects if obj != hitobj and obj.data]
+
+                            for obj in sel:
+                                if not obj.material_slots:
+                                    obj.data.materials.append(mat)
+
+                                else:
+                                    obj.material_slots[obj.active_material_index].material = mat
+
+
+                        bpy.ops.machin3.draw_label(text=mat.name, coords=self.mousepos, alpha=1, time=get_prefs().HUD_fade_material_picker)
+
+                    else:
+                        bpy.ops.machin3.draw_label(text="Empty", coords=self.mousepos, color=(0.5, 0.5, 0.5), alpha=1, time=get_prefs().HUD_fade_material_picker + 0.2)
+
+                else:
+                    bpy.ops.machin3.draw_label(text="None", coords=self.mousepos, color=(1, 0, 0), alpha=1, time=get_prefs().HUD_fade_material_picker + 0.2)
+
+                self.finish(context)
+                return {'FINISHED'}
+
+
+            # NAV PASSTHROUGH
+
+            elif event.type == 'MIDDLEMOUSE':
+                return {'PASS_THROUGH'}
+
+
+            # CANCEL
+
+            elif event.type in ['RIGHTMOUSE', 'ESC']:
+                self.finish(context)
+                return {'CANCELLED'}
 
         return {'RUNNING_MODAL'}
 
@@ -113,10 +177,25 @@ class MaterialPicker(bpy.types.Operator):
     def invoke(self, context, event):
 
         # init
-        self.is_assign = False 
+        self.assign = False 
+        self.assign_from_assetbrowser = False 
 
+        # init mouse cursor
+        init_cursor(self, event)
         context.window.cursor_set("EYEDROPPER")
-        self.mousepos = Vector((event.mouse_region_x, event.mouse_region_y))
+
+        # check the active screen and fetch all areas and their position/dimenension
+        self.areas, self.asset_browser = self.get_areas(context)
+
+        self.asset_browser_material = self.get_selected_material()
+
+        if self.asset_browser_material:
+            printd(self.asset_browser_material)
+
+        # printd(self.areas)
+        # print(self.asset_browser)
+
+        # return {'FINISHED'}
 
         self.dg = context.evaluated_depsgraph_get()
 
@@ -132,3 +211,66 @@ class MaterialPicker(bpy.types.Operator):
 
         context.window_manager.modal_handler_add(self)
         return {'RUNNING_MODAL'}
+
+    def get_areas(self, context):
+        '''
+        check the active screen and fetch all areas and their position/dimenension
+        '''
+
+        areas = {}
+        asset_browser = None
+
+        for area in context.screen.areas:
+            if area.type == 'FILE_BROWSER' and area.ui_type == 'ASSETS':
+                area_type = 'ASSET_BROWSER'
+                asset_browser = area.spaces.active.params
+
+            else:
+                area_type = area.type
+
+            # print("     x:", area.x)
+            # print("     y:", area.y)
+            # print(" width:", area.width)
+            # print("height:", area.height)
+
+            areas[area_type] = {'x': (area.x, area.x + area.width),
+                                'y': (area.y, area.y + area.height)}
+
+        return areas, asset_browser
+
+    def get_area_under_mouse(self, mousepos):
+        '''
+        return name of area, under ther mouse
+        '''
+
+        for areaname, coords in self.areas.items():
+            if coords['x'][0] <= self.mousepos_screen.x <= coords['x'][1]:
+                # print(" in x of", areaname)
+
+                if coords['y'][0] <= self.mousepos_screen.y <= coords['y'][1]:
+                    # print(" in y of", areaname, "too")
+                    return areaname
+
+    def get_selected_material(self):
+        '''
+        from the asset browser, fetch the selected material
+        '''
+
+        asset = None
+
+        if self.asset_browser:
+            ab = self.asset_browser
+            directory = ab.directory.decode()
+
+            asset = {'import_type': ab.import_type,
+                     'library': ab.asset_library_ref,
+                     'catalog_id': ab.catalog_id,
+                     'directory': directory,
+                     'filename': ab.filename}
+
+            # printd(asset)
+
+        else:
+            print("there is no asset browser")
+
+        return asset
