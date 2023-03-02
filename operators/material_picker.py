259ccfe6b2b56100c76264abf76eae13fce69550
machin3io <social@machin3.io>
2023-03-02 20:55:35 +0100

    MaterialPicker() - support assigning materials from the asset browser (5)
    * support assigning and assignming from asset browser to edit mode face selections

--- operators/material_picker.py
+++ operators/material_picker.py
@@ -1,6 +1,6 @@
 import bpy
 from bpy.props import BoolProperty
-from bl_ui.space_statusbar import STATUSBAR_HT_header as statusbar
+import bmesh
 from mathutils import Vector
 import os
 from .. utils.raycast import cast_obj_ray_from_mouse, cast_bvh_ray_from_mouse
@@ -63,7 +63,8 @@ class MaterialPicker(bpy.types.Operator):
 
     @classmethod
     def poll(cls, context):
-        return context.area.type == 'VIEW_3D'
+        if context.mode in ['OBJECT', 'EDIT_MESH']:
+            return context.area.type == 'VIEW_3D'
 
     def draw_HUD(self, context):
         draw_init(self, None)
@@ -118,8 +119,8 @@ class MaterialPicker(bpy.types.Operator):
             # fetch selected asset from asset browser
             self.asset = self.get_selected_asset()
 
-            if self.asset:
-                printd(self.asset)
+            # if self.asset:
+                # printd(self.asset)
 
 
         # PASSTROUGH to ASSET BROWSER
@@ -171,79 +172,88 @@ class MaterialPicker(bpy.types.Operator):
 
             # FINISH
 
-            elif event.type == 'LEFTMOUSE':
+            elif event.type == 'LEFTMOUSE' and event.value == 'PRESS':
 
                 hitobj, matindex = self.get_material_hit(context, self.mousepos, debug=False)
 
                 if hitobj:
 
-                    # PICK - make the hitobject and its material (slot) active
-
-                    context.view_layer.objects.active = hitobj
-                    hitobj.active_material_index = matindex
+                    # ASSIGN from ASSETBROWSER
 
                     if self.assign_from_assetbrowser:
 
-                        # Import Material from ASSETBROWSER and assign it to the picked object at matindex
+                        # Import Material from ASSETBROWSER and assign it to the picked object at matindex or to the selected faces in edit mode
 
                         if self.assign_from_assetbrowser:
                             if self.asset['asset_type'] == 'Material':
-                                import_type = self.asset['import_type']
-                                directory = os.path.join(self.asset['blendpath'], 'Material')
-                                filename = self.asset['asset_name']
 
-                                # print("\nassset browser material import")
-                                mat = bpy.data.materials.get(filename)
+                                # import material from assetbrowser, except when its in the scene already
+                                mat = self.get_material_from_assetbrowser(context)
 
-                                if mat:
-                                    # print(" a material of this name exists already:", mat)
-                                    pass
+                                # object mode - apply material at the matindex of the hitobject, and keep the modal going, as yo umay want to apply the material to other parts or objects
+                                if context.mode == 'OBJECT':
+                                    # print(" applying to obj", hitobj.name, "at index", matindex)
 
-                                else:
-                                    # print(" import type:", import_type)
-                                    # print(" directory:", directory)
-                                    # print(" filename:", filename)
-
-                                    if 'Append' in import_type:
-                                        reuse_local_id= 'Reuse' in import_type
-                                        bpy.ops.wm.append(directory=directory, filename=filename, do_reuse_local_id=reuse_local_id)
-
+                                    if hitobj.material_slots:
+                                        hitobj.material_slots[matindex].material = mat
                                     else:
-                                        bpy.ops.wm.link(directory=directory, filename=filename)
+                                        hitobj.data.materials.append(mat)
 
-                                    mat = bpy.data.materials.get(filename)
-                                    # print(" imported material:", mat)
+                                # edit mode - assign the material to active's face selection in edit mode, and FINISH
+                                elif context.mode == 'EDIT_MESH':
+                                    self.assign_material_in_editmode(context, mat)
 
-                                    # disable fake user
-                                    if mat.use_fake_user:
-                                        mat.use_fake_user = False
-
-                                # apply the material at the matindex
-                                print(" applying to obj", hitobj.name, "at index", matindex)
-                                hitobj.material_slots[matindex].material = mat
-
-                        # don't finish the modal in this case, finish using space instead
-                        return {'RUNNING_MODAL'}
+                                    self.finish(context)
+                                    return {'FINISHED'}
 
 
-                    # ASSIGN - assign the picked material to the selection of objects
+                    # ASSIGN - assign the picked material to the selection of objects, and FINISH
 
                     elif self.assign:
                         mat, matname = self.get_material_from_hit(hitobj, matindex)
 
                         if mat:
-                            sel = [obj for obj in context.selected_objects if obj != hitobj and obj.data]
 
-                            for obj in sel:
-                                if not obj.material_slots:
-                                    obj.data.materials.append(mat)
+                            # in object mode, assign the picked material to all objects in the selection at the active_material index
+                            if context.mode == 'OBJECT':
 
-                                else:
-                                    obj.material_slots[obj.active_material_index].material = mat
+                                sel = [obj for obj in context.selected_objects if obj != hitobj and obj.type == 'MESH']
 
-                self.finish(context)
+                                for obj in sel:
+                                    if not obj.material_slots:
+                                        obj.data.materials.append(mat)
 
-                return {'FINISHED'}
+                                    else:
+                                        obj.material_slots[obj.active_material_index].material = mat
+
+                            # in edit mode assign the picked material to the face selection, and we can simply reuse the function used by asset browser assigning here
+                            elif context.mode == 'EDIT_MESH':
+                                self.assign_material_in_editmode(context, mat)
+
+                        self.finish(context)
+
+                        return {'FINISHED'}
+
+
+                    # PICK - make the hitobject and its material (slot) active, and FINISH
+
+                    else:
+
+                        # if you are in edit mode and pick from an object that isnt in edit mode, you should take that one into edit mode
+                        # if you don't then changing the active pops the current object out of edit mode, which seems odd
+                        iseditmode = context.mode == 'EDIT_MESH'
+
+                        if context.active_object != hitobj:
+                            context.view_layer.objects.active = hitobj
+
+                            if iseditmode:
+                                bpy.ops.object.mode_set(mode='EDIT')
+                            
+                        # make the material slot on  the hitobject active, if you arn't assigning from the asset browser
+                        hitobj.active_material_index = matindex
+
+                        self.finish(context)
+                        return {'FINISHED'}
 
 
             # FINISH with SPACE
@@ -376,7 +386,7 @@ class MaterialPicker(bpy.types.Operator):
                      'asset_type': asset_type,
                      'asset_name': asset_name}
 
-            printd(asset)
+            # printd(asset)
 
         else:
             print("there is no asset browser")
@@ -395,7 +405,8 @@ class MaterialPicker(bpy.types.Operator):
             hitobj, hitobj_eval, _, _, hitindex, _ = cast_obj_ray_from_mouse(self.mousepos, depsgraph=self.dg, debug=False)
 
         elif context.mode == 'EDIT_MESH':
-            hitobj, _, _, hitindex, _, _ = cast_bvh_ray_from_mouse(self.mousepos, candidates=[obj for obj in context.visible_objects if obj.mode == 'EDIT'], debug=False)
+            # hitobj, _, _, hitindex, _, _ = cast_bvh_ray_from_mouse(self.mousepos, candidates=[obj for obj in context.visible_objects if obj.mode == 'EDIT'], debug=False)
+            hitobj, _, _, hitindex, _, _ = cast_bvh_ray_from_mouse(self.mousepos, candidates=[obj for obj in context.visible_objects], debug=False)
 
         if hitobj:
 
@@ -404,8 +415,7 @@ class MaterialPicker(bpy.types.Operator):
             elif context.mode == 'EDIT_MESH':
                 matindex = hitobj.data.polygons[hitindex].material_index
 
-            # if debug:
-            if True:
+            if debug:
                 print(" hit object:", hitobj.name, "material index:", matindex)
 
             # NOTE: safe-guard against crazy big material indices, which can happen as a result of (live?) booleans
@@ -428,3 +438,98 @@ class MaterialPicker(bpy.types.Operator):
                 mat = obj.material_slots[index].material
                 return mat, mat.name
         return None, 'None'
+
+    def get_material_from_assetbrowser(self, context):
+        import_type = self.asset['import_type']
+        directory = os.path.join(self.asset['blendpath'], 'Material')
+        filename = self.asset['asset_name']
+
+        # print("\nassset browser material import")
+        mat = bpy.data.materials.get(filename)
+
+        # only append/link if a material of that name doesn't exist already
+        if not mat:
+            # print(" import type:", import_type)
+            # print(" directory:", directory)
+            # print(" filename:", filename)
+
+            # cant append/link in edit mode, gotta switch to object mode
+            iseditmode = context.mode == 'EDIT_MESH'
+
+            if iseditmode:
+                bpy.ops.object.mode_set(mode='OBJECT')
+
+            if 'Append' in import_type:
+                reuse_local_id= 'Reuse' in import_type
+                bpy.ops.wm.append(directory=directory, filename=filename, do_reuse_local_id=reuse_local_id)
+
+            else:
+                bpy.ops.wm.link(directory=directory, filename=filename)
+
+            # switch back
+            if iseditmode:
+                bpy.ops.object.mode_set(mode='EDIT')
+
+            # fetch the imported material so it can be assigned
+            mat = bpy.data.materials.get(filename)
+            # print(" imported material:", mat)
+
+            # disable fake user
+            if mat.use_fake_user:
+                mat.use_fake_user = False
+
+        return mat
+
+    def assign_material_in_editmode(self, context, mat):
+        '''
+        apply material to face selection of active object
+        '''
+
+        active = context.active_object
+
+        if active.material_slots:
+            bm = bmesh.from_edit_mesh(active.data)
+            bm.normal_update()
+
+            faces = [f for f in bm.faces if f.select]
+
+            # only proceed if there is a face selection
+            if faces:
+                mat_indices = set(f.material_index for f in faces)
+
+                # chech if the material is already assigned to that material
+                if len(mat_indices) == 1:
+                    index = mat_indices.pop()
+                    mat_at_index = active.material_slots[index].material
+
+                    # not doing anything, the material is already assigned, so just ensure the index is active
+                    if mat_at_index == mat:
+                        active.active_material_index = index
+                        return
+
+                # check if the material is in the stack already, if so take the existing index
+                if mat.name in active.data.materials:
+                    index = list(active.data.materials).index(mat)
+
+                # get the index for the new material
+                else:
+                    index = len(active.material_slots)
+
+                for f in faces:
+                    f.material_index = index
+
+                bmesh.update_edit_mesh(active.data)
+
+                # run this too, otherwise the material picker won't recoginze the new material unless you finish and toggle modes
+                active.update_from_editmode()
+
+                # append the material if it isn't in the stack yet
+                if mat.name not in active.data.materials:
+                    active.data.materials.append(mat)
+
+                # make the new index active
+                active.active_material_index = index
+
+        # without any material slots, just append the material, to create the first slot
+        else:
+            active.data.materials.append(mat)
