2fd04c54205f3c381b72434609bff6e37ff0bcbb
machin3io <social@machin3.io>
2023-06-27 17:26:02 +0200

    bump version to 1.5.0

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 bl_info = {
     "name": "MACHIN3tools",
     "author": "MACHIN3, TitusLVR",
-    "version": (1, 4, 1),
+    "version": (1, 5, 0),
     "blender": (3, 3, 0),
     "location": "",
     "description": "Streamlining Blender 3.3+.",
