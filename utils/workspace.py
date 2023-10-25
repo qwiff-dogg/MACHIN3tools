6bdb743cf0e29bdf449d8333c3a43a43628fb43e
machin3io <social@machin3.io>
2023-10-25 14:39:04 +0200

    utils.workspace.get_3dview_area()
    * make sure context.workspace is not None before you proceeed to look for 3d view areas in workspace.screens
    * in 3.6 creating a HC asset causes workspace to be none, while the group handler is running
     - thereby interuppting the asset creation

--- utils/workspace.py
+++ utils/workspace.py
@@ -1,9 +1,13 @@
 
 def get_3dview_area(context):
-    for screen in context.workspace.screens:
-        for area in screen.areas:
-            if area.type == 'VIEW_3D':
-                return area
+    if context.workspace:
+        for screen in context.workspace.screens:
+            for area in screen.areas:
+                if area.type == 'VIEW_3D':
+                    return area
+
+    else:
+        print("WARNING: context has no workspace attribute")
 
 def get_3dview_space(area):
     for space in area.spaces:
