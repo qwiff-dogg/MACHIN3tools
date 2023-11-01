c303ed9c61a8e36bd675cb91bc3cb567804ccda9
machin3io <social@machin3.io>
2023-11-01 21:18:18 +0100

    uppdate filebrowser/assetbrowser tools for Blender 4.0
    * support opening the folder of an asset from the assetbrower in the system file browsr using o, just like in the filebrowser
    
    * to deal with API changes,
      - add utils.asset.get_asset_library_reference()
      - add utils.asset.set_asset_library_reference()
      - add utils.asset.get_asset_import_method()
      - add utils.asset.set_asset_import_method()

--- operators/filebrowser.py
+++ operators/filebrowser.py
@@ -3,6 +3,7 @@ from bpy.props import StringProperty, BoolProperty
 import os
 from .. utils.system import abspath, open_folder
 from .. utils.property import step_list
+from .. utils.asset import get_asset_library_reference, set_asset_library_reference, get_asset_import_method, set_asset_import_method, get_asset_details_from_space
 
 
 class Open(bpy.types.Operator):
@@ -18,12 +19,13 @@ class Open(bpy.types.Operator):
         return context.area.type == 'FILE_BROWSER'
 
     def execute(self, context):
-        params = context.space_data.params
+        space = context.space_data
+        params = space.params
+
         directory = abspath(params.directory.decode())
+        active_file = context.active_file
 
         if self.blend_file:
-            active_file = context.active_file
-
             if active_file.asset_data:
                 bpy.ops.asset.open_containing_blend_file()
 
@@ -32,7 +34,18 @@ class Open(bpy.types.Operator):
                 bpy.ops.machin3.open_library_blend(blendpath=path)
 
         else:
-            open_folder(directory)
+
+            # for the asset browser, fetch the library location via the custom get_asset_details_from_space() function
+            if active_file.asset_data:
+                _, libpath, _, _ = get_asset_details_from_space(context, space, debug=False)
+
+                if libpath:
+                    open_folder(libpath)
+
+
+            # for the filebrowser, you can just fetch it from params.directory
+            else:
+                open_folder(directory)
 
         return {'FINISHED'}
 
@@ -52,6 +65,7 @@ class Toggle(bpy.types.Operator):
         params = context.space_data.params
 
         # 1
+
         if self.type == 'SORT':
 
             # FILEBROWSER - toggle sorting by name or time
@@ -72,11 +86,14 @@ class Toggle(bpy.types.Operator):
                     base_libs.append('ESSENTIALS')
 
                 asset_libraries = base_libs + [lib.name for lib in context.preferences.filepaths.asset_libraries]
-                current = params.asset_library_ref
 
-                params.asset_library_ref = step_list(current, asset_libraries, 1)
+                current = get_asset_library_reference(params)
+                next = step_list(current, asset_libraries, 1)
+                set_asset_library_reference(params, next)
+
 
         # 2
+
         elif self.type == 'DISPLAY_TYPE':
 
             # FILEBROWSER - toggle display type
@@ -87,20 +104,25 @@ class Toggle(bpy.types.Operator):
                 else:
                     params.display_type = 'LIST_VERTICAL'
 
-            # ASSETBROWSER - cycle import types
+            # ASSETBROWSER - cycle import methods
             elif context.area.ui_type == 'ASSETS':
 
                 # only cycle importy types when you aren't in the LOCAL lib, in that case the prop is not used and is hidden in the UI, so you may end up changing it accidentally
-                if params.asset_library_ref != 'LOCAL':
-                    import_types = ['LINK', 'APPEND', 'APPEND_REUSE']
+                current = get_asset_library_reference(params)
+
+                if current != 'LOCAL':
+                    import_methods = ['LINK', 'APPEND', 'APPEND_REUSE']
 
                     if bpy.app.version >= (3, 5, 0):
-                        import_types.insert(0, 'FOLLOW_PREFS')
+                        import_methods.insert(0, 'FOLLOW_PREFS')
 
-                    current = params.import_type
-                    params.import_type = step_list(current, import_types, 1)
+                    current = get_asset_import_method(params)
+                    next = step_list(current, import_methods, 1)
+                    set_asset_import_method(params, next)
 
-        # 4 toggle hidden files in asset browser
+
+        # 4 toggle hidden files in file browser
+
         elif self.type == 'HIDDEN':
             if context.area.ui_type == 'FILES':
                 params.show_hidden = not params.show_hidden
@@ -122,8 +144,23 @@ class CycleThumbs(bpy.types.Operator):
         return context.area.type == 'FILE_BROWSER' and context.space_data.params.display_type == 'THUMBNAIL'
 
     # 3  
+
     def execute(self, context):
         params = context.space_data.params
-        sizes = ['TINY', 'SMALL', 'NORMAL', 'LARGE']
-        params.display_size = step_list(params.display_size, sizes, -1 if self.reverse else 1, loop=True)
+
+        if bpy.app.version >= (4, 0, 0):
+            if params.display_size == 256 and not self.reverse:
+                params.display_size = 16
+
+            elif params.display_size == 16 and self.reverse:
+                params.display_size = 256
+
+            else:
+                params.display_size += -20 if self.reverse else 20
+
+        else:
+            sizes = ['TINY', 'SMALL', 'NORMAL', 'LARGE']
+            params.display_size = step_list(params.display_size, sizes, -1 if self.reverse else 1, loop=True)
+
+
         return {'FINISHED'}
--- utils/asset.py
+++ utils/asset.py
@@ -4,6 +4,38 @@ from . system import printd
 from . registration import get_prefs
 
 
+# UTILS
+
+def get_asset_library_reference(params):
+    if bpy.app.version >= (4, 0, 0):
+        return params.asset_library_reference
+    else:
+        return params.asset_library_ref
+
+
+def set_asset_library_reference(params, name):
+    if bpy.app.version >= (4, 0, 0):
+        params.asset_library_reference = name
+    else:
+        params.asset_library_ref = name
+
+
+def get_asset_import_method(params):
+    if bpy.app.version >= (4, 0, 0):
+        return params.import_method
+    else:
+        return params.import_type
+
+
+def set_asset_import_method(params, name):
+    if bpy.app.version >= (4, 0, 0):
+        params.import_method = name
+    else:
+        params.import_type = name
+
+
+# CATALOGS
+
 def get_catalogs_from_asset_libraries(context, debug=False):
     '''
     scan cat files of all asset libraries and get the uuid for each catalog
@@ -58,43 +90,47 @@ def update_asset_catalogs(self, context):
     bpy.types.WindowManager.M3_asset_catalogs = bpy.props.EnumProperty(name="Asset Categories", items=items, default=default)
 
 
+# ASSET
+
 def get_asset_details_from_space(context, space, debug=False):
     '''
     fetch details of selected asset from the file/assetbrowser's space_data
     deal with ALL asset_library_ref, in which case the libpath (space.directory) will not be set, and the libname is not clear
         fetch both from the asset catalogs using the catalog_id in that case
 
-    return libname, libpath, filename and import_type
+    return libname, libpath, filename and import_method
     '''
 
     # debug = True
 
-    libref = space.params.asset_library_ref
+    lib_reference = get_asset_library_reference(space.params)
     catalog_id = space.params.catalog_id
-    libname = '' if libref == 'ALL' else libref
+    libname = '' if lib_reference == 'ALL' else lib_reference
     libpath = space.params.directory.decode('utf-8')
     filename = space.params.filename
-    import_type = space.params.import_type
+    import_method = get_asset_import_method(space.params)
 
     if debug:
         print()
-        print("asset_library_ref:", libref)
-        print("catalog_id:", catalog_id)
-        print("libname:", libname)
-        print("libpath:", libpath)
-        print("filename:", filename)
-        print("import_type:", import_type)
+        print("get_asset_details_from_space()")
+        print(" asset_library_reference:", lib_reference)
+        print(" catalog_id:", catalog_id)
+        print(" libname:", libname)
+        print(" libpath:", libpath)
+        print(" filename:", filename)
+        print(" import_method:", import_method)
+        print()
 
     if libname == 'LOCAL':
         if 'Object/' in filename:
-            return libname, libpath, filename, import_type
+            return libname, libpath, filename, import_method
 
     elif libname == 'ESSENTIALS':
         return None, None, None, None
 
     elif not libname and not libpath:
         if debug:
-            print("WARNING: asset library ref is ALL and directory is not set!")
+            print(" WARNING: asset library ref is ALL and directory is not set!")
 
         catalogs = get_catalogs_from_asset_libraries(context, debug=False)
 
@@ -104,12 +140,15 @@ def get_asset_details_from_space(context, space, debug=False):
                 libpath = catdata['libpath']
 
                 if debug:
-                    print("INFO: found libname and libpath via asset catalogs:", libname, "at", libpath)
+                    print(" INFO: found libname and libpath via asset catalogs:", libname, "at", libpath)
 
                 break
 
+    if debug:
+        print()
+
     if libpath:
-        return libname, libpath, filename, import_type
+        return libname, libpath, filename, import_method
 
     else:
         return None, None, None, None
